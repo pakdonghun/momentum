efedee4cccf0134b0e11f8de661783bc1b495a04
pakdonghun <kordn2004@gmail.com>
2022-03-18 22:54:30 +0900

    test

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-function plus(a,b){
-    console.log(a+b);
+const age = parseInt(prompt("How old are you?"));
+
+
+if(isNaN(age) || age < 0){
+    console.log("Please write a number");
+} else if (age < 18){
+    console.log("you are too young")
+} else if (age >= 18 && age <= 50){
+    console.log("You can drink")    
+} else{
+    console.log("You can't drink")
 }
-function divide(a,b){
-    console.log(a/b);
-}
-plus(8,60);
-divide(8,60);
