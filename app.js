37addd1c4dfad5b25a248ba7d92d316554fda8d5
pakdonghun <kordn2004@gmail.com>
2022-03-22 17:58:40 +0900

    commit

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector(".hello h1");
 
-
-if(isNaN(age) || age < 0){
-    console.log("Please write a number");
-} else if (age < 18){
-    console.log("you are too young")
-} else if (age >= 18 && age <= 50){
-    console.log("You can drink")    
-} else{
-    console.log("You can't drink")
-}
+//title.innerText = "bye"
