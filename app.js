4cfbf5d6ece4fe3582cda7e2964c534ddd33c209
pakdonghun <kordn2004@gmail.com>
2022-03-29 20:12:44 +0900

    commit

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-//title.innerText = "bye"
+function handleh1Click(){
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copy paste");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOOOD");
+}
+
+h1.addEventListener("click", handleh1Click);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
