1356f49d25572471ac7fc797738adc37b33dd000
pakdonghun <kordn2004@gmail.com>
2022-03-29 22:40:42 +0900

    commit

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
-const h1 = document.querySelector(".hello h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleh1Click(){
-    h1.style.color = "blue";
+function onLoginBtnClick(){
+    console.log(loginInput.value);
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copy paste");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOOOD");
-}
-
-h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+loginButton.addEventListener("click", onLoginBtnClick);
