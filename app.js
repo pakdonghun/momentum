b7004ea452dc7d07dbd68f7b3fe64b295de75125
pakdonghun <kordn2004@gmail.com>
2022-03-31 19:08:35 +0900

    commit

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
-}
-
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
 }
 
 loginForm.addEventListener("submit" , onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
