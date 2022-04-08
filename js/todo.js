8e9856a2e3befa5ecc8b6b536c9e5fa6a72fff98
pakdonghun <kordn2004@gmail.com>
2022-04-08 20:12:06 +0900

    commit

--- js/todo.js
+++ js/todo.js
@@ -2,18 +2,27 @@
  const toDoInput = document.querySelector("#todo-form input");
  const toDoList = document.getElementById("todo-list");
 
+ function deleteTodo(event){
+     const li = event.target.parentElement;
+     li.remove();
+ }
+
  function paintToDo(newTodo){
      const li = document.createElement("li");
      const span = document.createElement("span");
-     li.appendChild(span);
      span.innerText = newTodo;
+     const button = document.createElement("button");
+     button.innerText = "❌"
+     button.addEventListener("click", deleteTodo);
+     li.appendChild(span);
+     li.appendChild(button);
      toDoList.appendChild(li);
  }
 
  function handleToDoSubmit(event){
      event.preventDefault();
      const newTodo = toDoInput.value;
-     toDoInput,value = "";
+     toDoInput.value = "";
      paintToDo(newTodo);
  }
  
